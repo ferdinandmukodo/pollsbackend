10324a69b294e573304b63d9e0c521b2de946f9f
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-12-08 06:51:47 +0300

    Debugging

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/signup", to: "users#create"
+  get "me", to: "users#show"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :polls, only: [:index, :show] do
+    resources :questions, only: [:create, :destroy, :update, :index]
+  end
 end
