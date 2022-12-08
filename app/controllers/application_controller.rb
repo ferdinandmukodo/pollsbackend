68a4247ceacac6d67a9d06963d68a98b2c853ad4
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-12-08 08:17:56 +0300

    user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::Base
-end
+class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    before_action :authorized
+  
+    def authorized
+      @current_user = User.find(session[:user_id])
+      render json: { errors: ["Not authorized "] } unless session.include? :user_id
+    end
+  end
